bc6702d052a328d6ec7c9bf35227841ea134d1fc
fengyangcao <903775389@qq.com>
2019-11-11 10:34:39 +0800

    init proj

---
+++ FYDataStructureDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FYDataStructureDemo
+//
+//  Created by admin on 2019/11/11.
+//  Copyright © 2019 fengyangcao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
